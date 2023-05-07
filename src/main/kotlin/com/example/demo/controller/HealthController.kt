766fccf93cfe3bbaab276aec45636b8c6f6366f5
konempty <jameswebb@naver.com>
2023-05-07 13:23:07 +0900

    HealthController

---
+++ src/main/kotlin/com/example/demo/controller/HealthController.kt
@@ -0,0 +1,25 @@
+package com.example.demo.controller
+
+
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.context.annotation.Profile
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Profile("!test")
+@RestController
+class HealthController(
+) {
+    @GetMapping("/health")
+    fun health(): HealthResponse {
+        return HealthResponse(status = HttpServletResponse.SC_OK)
+    }
+
+    @GetMapping("/ready")
+    fun ready() {
+    }
+}
+
+data class HealthResponse(
+    val status: Int,
+)
